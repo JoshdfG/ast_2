54249a4ccbd84bf0f8e7a474567aa3b64e3b8547
joshdfg <slimmy0310@gmail.com>
2024-08-03 18:25:35 -0400

    feat: stored the html elements in a library

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use lib::HTML_ELEMENTS;
 fn main() {
     for html in HTML_ELEMENTS {
         let mut result = String::new();
-        let mut in_tag = false;
+        let mut opening_tag = false;
 
         for char in html.chars() {
             match char {
-                '<' => in_tag = true,
-                '>' => in_tag = false,
-                _ if in_tag => (),
+                '<' => opening_tag = true,
+                '>' => opening_tag = false,
+                _ if opening_tag => (),
                 _ => result.push(char),
             }
         }
